907f5e42464ce6b949e80d5cb549f6f8b30cb884
Vinicius Pinheiro <vinicius.pinheiro@dxc.com>
2023-06-08 12:20:09 -0300

    Starting projects

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 08 Jun 2023 14:20:38 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorZinc400": "#a1a1aa",
+  "ColorZinc600": "#52525b",
+  "ColorZinc800": "#27272a",
+  "ColorZinc900": "#18181b",
+  "ColorViolet500": "#8b5cf6"
+};
